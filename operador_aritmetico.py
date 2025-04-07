4000827eda37ebfbd254726962c82b543668c3f7
Romanvark <jrromero@uamv.edu.ni>
2025-04-07 12:37:55 -0600

    fisrt commit

---
+++ operador_aritmetico.py
@@ -0,0 +1,5 @@
+#suma de dos numeros
+numero1=int(input("Ingrese el primer numero: "))
+numero2=int(input("Ingrese el segundo numero: "))
+suma=numero1+numero2
+print(f"La suma de \n  {numero1:>9,} \n+ {numero2:>9,} \n= {suma:>9,}")
---
+++ operador_relacional.py
@@ -0,0 +1,10 @@
+num1=25
+num2=25
+print(num1>num2)
+print(num1<num2)
+print(num1==num2)
+print(num1!=num2)
+print(num1>=num2)
+print(num1<=num2)
+print(not(num1)==num2)
+print(num1 is not num2)
