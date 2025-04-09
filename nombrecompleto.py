06e62c3e1bf80f34a7f1c6f44d672d6cd1aee267
Romanvark <jrromero@uamv.edu.ni>
2025-04-09 11:01:05 -0600

    nombre comleto de la guia

---
+++ nombrecompleto.py
@@ -0,0 +1,7 @@
+nombre = input("¿Cuál es tu nombre? ")
+apellido = input("¿Cuál es tu apellido? ")
+nombre_completo = nombre + " " + apellido
+print("Tu nombre completo es:", nombre_completo)
+print(f"Tu nombre completo es: {nombre_completo}")
+# print(f"Tu nombre completo es: {nombre} {apellido}")
+print(f'Tu nombre completo es: {nombre} {apellido}')
